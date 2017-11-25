aaefde147b36735208f410ace757bb407ffea54c
naronA <otherthan.new@gmail.com>
2017-11-25 14:49:19 +0900

    tqdm対応/コマンドライン引数対応/画像URL取得と画像取得のループを分ける

--- scrape/instagram.py
+++ scrape/instagram.py
@@ -2,22 +2,27 @@ import json
 import os
 import requests
 import time
+import sys
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 INSTAGRAM_URL = 'https://www.instagram.com/'
 FAMOUS_ACCOUNTS = {'lavie_city', 'starbucks_j',
-                   'estyle1010', 'feel_kiyomizudera'}
+                   'estyle1010', 'keiyamazaki',
+                   'feel_kiyomizudera', 'wat.ki'}
 PHOTOGENIC_DIR = 'photogenic'
 
 
-def scrape(acc_name):
-    if not os.path.isdir(PHOTOGENIC_DIR):
-        os.mkdir(PHOTOGENIC_DIR)
+def scrape(acc_name, base_dir):
+    acc_dir = '{}/{}'.format(base_dir, acc_name)
+    if not os.path.isdir(acc_dir):
+        os.mkdir(acc_dir)
 
     acc_url = INSTAGRAM_URL + acc_name
     next_page_url = acc_url
 
     has_next_page = True
+    src_urls = []
     while has_next_page:
         resp = requests.get(next_page_url)
         bs = BeautifulSoup(resp.content, 'html.parser')
@@ -27,21 +32,35 @@ def scrape(acc_name):
         medias = loaded['entry_data']['ProfilePage'][0]['user']['media']
         nodes = medias['nodes']
         for n in nodes:
-            for res in n['thumbnail_resources']:
+            thumnails = n['thumbnail_resources']
+            for res in thumnails:
                 if res['config_height'] == 640:
-                    photo_url = res['src']
-                    filename = photo_url.split('/')[-1]
-                    filepath = '{}/{}'.format(PHOTOGENIC_DIR, filename)
-                    photo = requests.get(photo_url)
-                    with open(filepath, 'wb') as f:
-                        f.write(photo.content)
-                    time.sleep(1)
+                    src_urls.append(res['src'])
+
         # 次のページを取得
         page_info = medias['page_info']
         has_next_page = page_info['has_next_page']
         if has_next_page:
             end_cursor = page_info['end_cursor']
-            next_page_url = '{}/?max_id={}'.format(acc_url, end_cursor)
+        next_page_url = '{}/?max_id={}'.format(acc_url, end_cursor)
+        time.sleep(0.5)
+
+    for src in tqdm(src_urls, ncols=100, ascii=True):
+        filename = src.split('/')[-1]
+        filepath = '{}/{}'.format(acc_dir, filename)
+        img = requests.get(src)
+        with open(filepath, 'wb') as f:
+            f.write(img.content)
+        time.sleep(1)
 
 
-scrape('lavie_city')
+if not os.path.isdir(PHOTOGENIC_DIR):
+    os.mkdir(PHOTOGENIC_DIR)
+
+args = sys.argv
+if len(args) >= 1:
+    exit
+
+acc_name = args[1:]
+for acc in acc_name:
+    scrape(acc, PHOTOGENIC_DIR)
