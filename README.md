33765ea1c8ba1ff3cf870e9ed0393f85a0e077d8
naronA <otherthan.new@gmail.com>
2018-01-17 10:11:15 +0900

    READMEとsettingsに説明を追加する

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Instagram Scaper
 
 設定したアカウントの画像ファイルをスクレイピングします。
+指定したアカウントをページネーションして全画像を取得します。
+
+## 設定
+
+settings.pyを編集して設定変更できます
+
+## 実行
 
 ``` bash
 python instagram.py
---
+++ settings.py
@@ -0,0 +1,11 @@
+# 画像の保存先
+SAVE_DIR = 'photo'
+
+# スクレイピングする画像のピクセルの縦横サイズ
+PIXEL_SIZE = 640  # 150 240 320 480
+
+# スクレイピング対処のアカウント
+ACCOUNTS = {'lavie_city', 'starbucks_j',
+            'estyle1010', 'keiyamazaki',
+            'feel_kiyomizudera', 'wat.ki',
+            'airio830'}
